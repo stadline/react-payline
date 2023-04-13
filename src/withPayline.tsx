46335bd42e12dd690b09a8d550f990a041c043e5
Antoine <antoine.lucas@nickel.eu>
2023-04-13 17:16:27 +0200

    Improve withPayline types

--- src/withPayline.tsx
+++ src/withPayline.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import PaylineProvider from './PaylineProvider';
 
-const withPayline = (production: boolean) => (WrappedComponent: React.ComponentType) => (props: any) => (
+const withPayline = (production: boolean) => <TProps,>(WrappedComponent: React.ComponentType<TProps>) => (props: PropsWithChildren<TProps>) => (
   <PaylineProvider production={production}>
     <WrappedComponent {...props} />
   </PaylineProvider>
