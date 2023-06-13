3eeabd67f0348d81fc7e8a0143bfa6af4bd658b8
Alexandre Paixao <alexandre.paixao@stadline.com>
2023-06-13 12:41:10 +0200

    Alternative way to check payline loading

--- src/usePayline.ts
+++ src/usePayline.ts
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 // See https://docs.payline.com/display/DT/API+JavaScript for full documentation of Payline API
 type PaylineApi = {
   endToken: (
@@ -38,8 +40,13 @@ declare global {
 export const usePayline = () => {
   if (typeof window === 'undefined') return undefined;
 
-  if (!window.Payline)
-    throw new Error('window.Payline is unavailable. Check if PaylineProvider is rendered within the component tree.');
+  useEffect(() => {
+    if (!window.Payline && !document.querySelector('script[src$="payline.com/cdn/scripts/widget-min.js"]'))
+      throw new Error(
+        "window.Payline is unavailable. Check if PaylineProvider is rendered within the component tree."
+      );
+  }, []);
+
   return window.Payline;
 };
 
